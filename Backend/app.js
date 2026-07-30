737e5cf544df335c5e2ce7431e2e3ec766059258
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-07-30 20:34:33 +0530

    e-commerce user/router

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+require("./config/db");
+const PORT = process.env.PORT || 5000 ; 
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+});
