7ca153b30f97cc35afa156618fb0302afde5ddb6
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-09-10 20:55:13 +0530

    backend

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,4 +1,40 @@
+import { useState } from 'react';
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+
+
 function Login() {
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Login form submit
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post(
+                `${process.env.REACT_APP_API}/api/auth/login`,
+                {
+                    email,
+                    password
+                }
+            );
+
+            if (res.data.success) {
+                toast.success(res.data.message);
+                // redirect
+                navigate("/");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong");
+        }
+    };
+
     return (
         <div>
 
@@ -15,16 +51,37 @@ function Login() {
                                             </a>
                                         </div>
                                         <h3 className="rbt-title rbt-text-bold mb--16 h6">Sign In To Proceed</h3>
-                                        <form>
+                                        <form onSubmit={handleSubmit}>
                                             <div className="mb-3">
+                            
                                                 <label htmlFor="email" className="form-label">Email address</label>
-                                                <input type="email" className="form-control" id="email" placeholder="Enter your email" required />
+                                                <input
+                                                    type="email"
+                                                    className="form-control"
+                                                    id="email"
+                                                    placeholder="Enter your email"
+                                                    name='email'
+                                                    value={email}
+                                                    onChange={(e) => setEmail(e.target.value)}
+                                                    required
+                                                />
                                             </div>
+
                                             <div className="mb-3">
+
                                                 <label htmlFor="password" className="form-label">Password</label>
-                                                <input type="password" className="form-control" id="password" placeholder="Enter your password" required />
+                                                <input
+                                                    type="password"
+                                                    className="form-control"
+                                                    id="password"
+                                                    placeholder="Enter your password"
+                                                    name='password'
+                                                    value={password}
+                                                    onChange={(e) => setPassword(e.target.value)}
+                                                    required
+                                                />
                                             </div>
-                                            
+
                                             <button type="submit" className="btn btn-primary w-100">
                                                 Login
                                             </button>
